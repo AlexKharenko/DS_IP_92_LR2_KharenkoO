b6bec3ab88e4ec822356f9fad8e7899d29474828
Alexey Harenko <alexey16052002@gmail.com>
2020-02-24 19:55:46 +0200

    Add read from file

--- DS_IP_92_LR2_KharenkoO/DS_IP_92_LR2_KharenkoO.cpp
+++ DS_IP_92_LR2_KharenkoO/DS_IP_92_LR2_KharenkoO.cpp
@@ -1,8 +1,57 @@
-﻿#include <iostream>
+﻿#include <fstream>
+#include <iostream>
+
+using namespace std;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    ifstream in("not_orient.txt");
+    if (in.is_open()) {
+        int count = 0, temp, count_space = 0;
+        char symb;
+        while (!in.eof()) {
+            in >> temp;
+           
+            count++;
+        }
+        in.seekg(0, ios::beg);
+        in.clear();
+        while (!in.eof()) {
+            
+            in.get(symb);
+            if (symb == ' ') {
+                count_space++;
+            }
+            if (symb == '\n') {
+                break;
+            }
+            
+        }
+        in.seekg(0, ios::beg);
+        in.clear();
+        int l1 = count / (count_space + 1);
+        int l2 = count_space + 1;
+        int** a = new int* [l1];
+        for (int i = 0; i < l1; i++) {
+            a[i] = new int[l2];
+        }
+        for (int i = 0; i < l1; i++) {
+            for (int j = 0; j < l2; j++) {
+                in >> a[i][j];
+            }
+        }
+        for (int i = 0; i < l1; i++) {
+            for (int j = 0; j < l2; j++) {
+                cout << a[i][j]<<" ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+    else {
+        cout << "File isn't opened!" << endl;
+    }
 }
 
 
